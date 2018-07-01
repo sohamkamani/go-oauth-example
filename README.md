64329e50d948df68d62a90b61c01971bcb91872b
Soham Kamani <sohamkamani@gmail.com>
2018-07-02 00:27:53 +0530

    working example

---
+++ README.md
@@ -0,0 +1,12 @@
+## Node.js OAuth example
+
+Read the blog post [here](https://www.sohamkamani.com/blog/golang/2018-06-24-oauth-with-golang/)
+
+This is an example node application that implements Githubs OAuth2 API.
+
+In order to run the application:
+
+1. Register your new application on Github : https://github.com/settings/applications/new. In the "callback URL" field, enter "http://localhost:8080/oauth/redirect". Once you register, you will get a client ID and client secret.
+2. Replace the values of the `clientID` and `clientSecret` variables in the [main.go](/main.go) file 
+4. Start the server by executing `go run main.go`
+5. Navigate to http://localhost:8080 on your browser.
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+const clientID = "<your client id>"
+const clientSecret = "<your client secret>"
+
+func main() {
+	fs := http.FileServer(http.Dir("public"))
+	http.Handle("/", fs)
+
+	// We will be using `httpClient` to make external HTTP requests later in our code
+	httpClient := http.Client{}
+
+	// Create a new redirect route route
+	http.HandleFunc("/oauth/redirect", func(w http.ResponseWriter, r *http.Request) {
+		// First, we need to get the value of the `code` query param
+		err := r.ParseForm()
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "could not parse query: %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		code := r.FormValue("code")
+
+		// Next, lets for the HTTP request to call the github oauth enpoint
+		// to get our access token
+		reqURL := fmt.Sprintf("https://github.com/login/oauth/access_token?client_id=%s&client_secret=%s&code=%s", clientID, clientSecret, code)
+		req, err := http.NewRequest(http.MethodPost, reqURL, nil)
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "could not create HTTP request: %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		// We set this header since we want the response
+		// as JSON
+		req.Header.Set("accept", "application/json")
+
+		// Send out the HTTP request
+		res, err := httpClient.Do(req)
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "could not send HTTP request: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		defer res.Body.Close()
+
+		// Parse the request body into the `OAuthAccessResponse` struct
+		var t OAuthAccessResponse
+		if err := json.NewDecoder(res.Body).Decode(&t); err != nil {
+			fmt.Fprintf(os.Stdout, "could not parse JSON response: %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+		}
+
+		// Finally, send a response to redirect the user to the "welcome" page
+		// with the access token
+		w.Header().Set("Location", "/welcome.html?access_token="+t.AccessToken)
+		w.WriteHeader(http.StatusFound)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
+
+type OAuthAccessResponse struct {
+	AccessToken string `json:"access_token"`
+}
